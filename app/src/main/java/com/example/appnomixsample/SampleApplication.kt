e22b672313edb4f097a1b763eeb975a1828b450f
mariusg <marius.gherman@appnomix.app>
2025-11-20 14:12:32 +0200

    bump Appnomix SDK to 2.0.1

--- app/src/main/java/com/example/appnomixsample/SampleApplication.kt
+++ app/src/main/java/com/example/appnomixsample/SampleApplication.kt
@@ -1,16 +1,16 @@
 package com.example.appnomixsample
 
 import android.app.Application
-import app.appnomix.sdk.external.AppnomixSdkFacade
+import app.appnomix.sdk.external.AppnomixCSDK
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppnomixSdkFacade.setup(
-            AppnomixSdkFacade.Config(
-                authToken = "01K1ZF0V4X8E6V71EYM2XBQTXT",
-                clientId = "01K1ZF2DF4WTYXDHGCW4KV7RKF",
+        AppnomixCSDK.initialize(
+            clientId = "YOUR_CLIENT_ID",
+            authToken = "YOUR_AUTH_TOKEN",
+            options = AppnomixCSDK.ConfigurationOptions(
                 language = "de"
             )
         )
